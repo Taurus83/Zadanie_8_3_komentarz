b96f7dfde7e5d64583440b4d987cd96837ab08f3
John Doe <john.doe@example.com>
2017-04-22 16:36:52 +0200

    Dodałem komentarz do skryptu

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var name = prompt('Wpisz swoje imię');
+// komentarz blokowy do polecenia alert
+// alert('Witaj, ' + name);
+// funkcja inicjuje okienko na stronie z pytaniem o imię użykownika strony
+alert('Witaj, ' + name);
+/*
+  komentarz blokowy do polecenia console
+  console.log('Witaj, ' + name);
+  funkcja działa tak samo jak alert('Witaj, ' + name);
+  funkcja pozwala na monitorowanie z poziomu konsoli
+*/
+console.log('Witaj, ' + name);
